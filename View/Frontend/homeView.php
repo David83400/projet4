7400787ae8f574963d7aeb779b626135ab4bd140
David Roche <davidroche83400@gmail.com>
2020-05-26 16:16:42 +0200

    Visuel de la home Page

--- View/Frontend/homeView.php
+++ View/Frontend/homeView.php
@@ -1,13 +1,130 @@
 <?php $this->title = 'Jean Forteroche | Blog officiel' ?>
 
+<section id="slider">
+  <div></div>
+  <div>
+      <a href="#"><h2>Un récit jubilatoire rempli d'amour et de partage</h2></a>
+  </div>
+  <div></div>
+</section>
+<section>
+  <article>
+    <h3>Chers visiteurs,</h3>
+    <p>J'ai voulu partager avec vous l'écriture de mon nouveau roman dans son format numérique épisode après épisode. Nous pourrons ainsi voyager ensemble dans des contrées merveilleuses. Vous partagerez avec moi les aventures extraordinaires de Balthazar en Alaska.
+    Bonne lecture à vous tous.
+    </p>
+    <p>Jean Forteroche</p>
+  </article>
+</section>
 <?php foreach ($lastEpisode as $lastEpisode): ?>
 <article>
   <header>
     <a href="<?= "index.php?action=episode&id=" . htmlspecialchars($lastEpisode['id']) ?>">
       <h1><?= htmlspecialchars($lastEpisode['title']) ?></h1>
     </a>
-    <p>Publié par Jean Forteroche le : <?= htmlspecialchars($lastEpisode['creationFrDate']) ?></p>
+    <p>Publié par Jean Forteroche le <?= htmlspecialchars($lastEpisode['creationFrDate']) ?></p>
   </header>
-  <p><?= htmlspecialchars($lastEpisode['content']) ?></p>
+  <p><?= htmlspecialchars(substr($lastEpisode['content'], 0, 250)) . ' ...' . '<a href="#">lire la suite</a>' ?></p>
 </article>
 <?php endforeach; ?>
+<section>
+  <h2>Mes derniers récits</h2>
+  <ul>
+    <li>
+      <figure>
+        <a href="#"><img src="Public/images/aLaRechercheDuBonheur.png" alt="livre de Jean Forteroche" /></a>
+        <figcaption>
+
+        </figcaption>
+      </figure>
+    </li>
+    <li>
+      <figure>
+        <a href="#"><img src="Public/images/elleALaPecheMamie.png" alt="livre de Jean Forteroche" /></a>
+        <figcaption>
+
+        </figcaption>
+      </figure>
+    </li>
+    <li>
+      <figure>
+        <a href="#"><img src="Public/images/elleEstDailleurs.png" alt="livre de Jean Forteroche" /></a>
+        <figcaption>
+
+        </figcaption>
+      </figure>
+    </li>
+    <li>
+      <figure>
+        <a href="#"><img src="Public/images/jeLaimeAMourir.png" alt="livre de Jean Forteroche" /></a>
+        <figcaption>
+
+        </figcaption>
+      </figure>
+    </li>
+    <li>
+      <figure>
+        <a href="#"><img src="Public/images/leHurlement.png" alt="livre de Jean Forteroche" /></a>
+        <figcaption>
+
+        </figcaption>
+      </figure>
+    </li>
+    <li>
+      <figure>
+        <a href="#"><img src="Public/images/jeLuiDiraiDesMotsBleus.png" alt="livre de Jean Forteroche" /></a>
+        <figcaption>
+
+        </figcaption>
+      </figure>
+    </li>
+    <li>
+      <figure>
+        <a href="#"><img src="Public/images/jiraiTeDecrocherLaLune.png" alt="livre de Jean Forteroche" /></a>
+        <figcaption>
+
+        </figcaption>
+      </figure>
+    </li>
+    <li>
+      <figure>
+        <a href="#"><img src="Public/images/laTeteDansLesEtoiles.png" alt="livre de Jean Forteroche" /></a>
+        <figcaption>
+
+        </figcaption>
+      </figure>
+    </li>
+    <li>
+      <figure>
+        <a href="#"><img src="Public/images/lesRevesDeSophie.png" alt="livre de Jean Forteroche" /></a>
+        <figcaption>
+
+        </figcaption>
+      </figure>
+    </li>
+    <li>
+      <figure>
+        <a href="#"><img src="Public/images/unTangoAHuwan.png" alt="livre de Jean Forteroche" /></a>
+        <figcaption>
+
+        </figcaption>
+      </figure>
+    </li>
+    <li>
+      <figure>
+        <a href="#"><img src="Public/images/vousEtesTresBeau.png" alt="livre de Jean Forteroche" /></a>
+        <figcaption>
+
+        </figcaption>
+      </figure>
+    </li>
+    <li>
+      <figure>
+        <a href="#"><img src="Public/images/wilfriedEtLesLucioles.png" alt="livre de Jean Forteroche" /></a>
+        <figcaption>
+
+        </figcaption>
+      </figure>
+    </li>
+  </ul>
+</section>
--- View/template.php
+++ View/template.php
@@ -4,9 +4,40 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="Public/style.css" />
+        <script src="https://kit.fontawesome.com/c9ef589bf6.js" crossorigin="anonymous"></script>
         <title><?= $title ?></title>
     </head>
     <body>
-        <?= $content ?>
+        <header>
+            <h1>Jean Forteroche</h1>
+            <ul>
+                <li><i class="fab fa-facebook-f"></i></li>
+                <li><i class="fab fa-twitter"></i></li>
+                <li><i class="fab fa-instagram"></i></li>
+            </ul>
+        </header>
+        <nav>
+            <ul>
+                <li><a href="#"><i class="fas fa-home"></i></a></li>
+                <li><a href="#">L'auteur</a></li>
+                <li><a href="#">Les romans</a></li>
+                <li><a href="#">Billet simple</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+            <?= $content ?>
+        <ul>
+            <li><i class="fab fa-facebook-f"></i></li>
+            <li><i class="fab fa-twitter"></i></li>
+            <li><i class="fab fa-instagram"></i></li>
+        </ul>
+        <footer>
+            <ul>
+                <li><a href="#">Mentions légales</a></li>
+                <li><a href="#">politique de confidentialité</a></li>
+                <li><a href="#">Cookies</a></li>
+            </ul>
+        </footer>
     </body>
 </html>
+
