37ef2d50f6f9e0805ac9c8524d1e721ea36fd5c2
David Roche <davidroche83400@gmail.com>
2020-06-02 18:42:49 +0200

    Visuel page d'accueil et liste épisodes

--- View/Frontend/episodesView.php
+++ View/Frontend/episodesView.php
@@ -1,13 +1,22 @@
 <?php $this->title = 'Jean Forteroche | Billet simple pour l\'Alaska' ?>
 
 <?php foreach ($episodes as $episode): ?>
-    <article>
-        <header>
-            <a href="<?= "index.php?action=episode&id=" . htmlspecialchars($episode['id']) ?>">
-                <h1><?= htmlspecialchars($episode['title']) ?></h1>
-            </a>
-            <p>Publié par Jean Forteroche le <?= htmlspecialchars($episode['creationFrDate']) ?></p>
-        </header>
-        <p><?= htmlspecialchars($episode['content']) ?></p>
-    </article>
+<section id="episodesList">
+    <div class="card mb-3">
+        <div class="row no-gutters">
+            <div class="col-md-3 col-lg-3 offset-lg-1">
+                <img src="Public/images/couverture.jpg" class="card-img" alt="livre de Jean Forteroche">
+            </div>
+            <div class="col-md-3 col-md-7 offset-md-2 col-lg-7 offset-lg-1">
+                <div class="card-body">
+                    <div class="card-title">
+                        <h1><?= htmlspecialchars($episode['title']) ?></h1>
+                    </div>
+                    <p class="card-text"><?= htmlspecialchars(substr($episode['content'], 0, 550)) ?> ... <a href="<?= 'index.php?action=episode&id=' . htmlspecialchars($episode['id']) ?>">lire la suite</a></p>
+                    <p class="card-text"><small class="text-muted">Publié par Jean Forteroche le <?= htmlspecialchars($episode['creationFrDate']) ?></small></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 <?php endforeach; ?>
--- View/Frontend/homeView.php
+++ View/Frontend/homeView.php
@@ -1,130 +1,138 @@
 <?php $this->title = 'Jean Forteroche | Blog officiel' ?>
 
-<section id="slider">
-  <div></div>
-  <div>
-      <a href="#"><h2>Un récit jubilatoire rempli d'amour et de partage</h2></a>
+<section>
+  <div class="row slider">
+    <div class="col-lg-4 blocImgAuthor">
+      <img src="Public/images/jeanForteroche.png" alt="Jean Forteroche auteur" class="imgAuthor">
+    </div>
+    <div class="col-6 offset-3 col-sm-6 offset-sm-3 col-md-5 offset-md-1 col-lg-3 offset-lg-0 quoteNews">
+      <blockquote>
+        <h2><i class="fas fa-quote-left"></i><a href="#"><span> Un récit jubilatoire rempli d'amour et de partage </span></a><i class="fas fa-quote-right"></i></h2>
+      </blockquote>
+    </div>
+    <div class="col-6 offset-2 col-sm-4 offset-sm-3 col-md-4 offset-md-1 col-lg-4 offset-lg-0 blocBilletSimple">
+      <img src="Public/images/billetSimple.png" alt="livre de Jean Forteroche" class="imgBilletSimple">
+    </div>
   </div>
-  <div></div>
 </section>
 <section>
-  <article>
-    <h3>Chers visiteurs,</h3>
-    <p>J'ai voulu partager avec vous l'écriture de mon nouveau roman dans son format numérique épisode après épisode. Nous pourrons ainsi voyager ensemble dans des contrées merveilleuses. Vous partagerez avec moi les aventures extraordinaires de Balthazar en Alaska.
-    Bonne lecture à vous tous.
-    </p>
-    <p>Jean Forteroche</p>
-  </article>
+  <div class="row presentation">
+    <article class="introduction col-8 offset-2 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-5 offset-lg-4 col-xl-4 offset-xl-4">
+      <p>Chers visiteurs,</p>
+      <p>J'ai voulu partager avec vous l'écriture de mon nouveau roman dans son format numérique épisode après épisode. Nous pourrons ainsi voyager ensemble dans des contrées merveilleuses. Vous partagerez avec moi les aventures extraordinaires de Balthazar en Alaska.
+      Bonne lecture à vous tous.
+      </p>
+      <p>Jean Forteroche.</p>
+    </article>
+  </div>
 </section>
 <?php foreach ($lastEpisode as $lastEpisode): ?>
-<article>
-  <header>
-    <a href="<?= "index.php?action=episode&id=" . htmlspecialchars($lastEpisode['id']) ?>">
-      <h1><?= htmlspecialchars($lastEpisode['title']) ?></h1>
-    </a>
-    <p>Publié par Jean Forteroche le <?= htmlspecialchars($lastEpisode['creationFrDate']) ?></p>
-  </header>
-  <p><?= htmlspecialchars(substr($lastEpisode['content'], 0, 250)) . ' ...' . '<a href="#">lire la suite</a>' ?></p>
+<article id="lastEpisode">
+  <div class="row">
+    <div class="col-sm-3 offset-sm-6 col-md-3 offset-md-6 col-lg-3 offset-lg-6 book">
+      <div class="front">
+        <div class="title">
+          <h2>chapitre n°1</h2>
+        </div>
+      </div>
+      <div class="layer1"></div>
+      <div class="layer2"></div>
+      <div class="layer3"></div>
+      <div class="layer4"></div>
+      <div class="layer-text">
+        <div class="page-text">
+          <header>
+            <h3><?= htmlspecialchars($lastEpisode['title']) ?></h3>
+            <p>Publié par Jean Forteroche le <?= htmlspecialchars($lastEpisode['creationFrDate']) ?></p>
+          </header>
+        </div>
+      </div>
+      <div class="layer-text-2">
+        <div class="page-text-2">
+          <p><?= htmlspecialchars(substr($lastEpisode['content'], 0, 550)) ?> ... <a href="<?= 'index.php?action=episode&id=' . htmlspecialchars($lastEpisode['id']) ?>">lire la suite</a></p>
+        </div>
+      </div>
+      <div class="layer5"></div>
+      <div class="layer6"></div>
+      <div class="layer7"></div>
+      <div class="layer8"></div>
+      <div class="back"></div>
+    </div>
+  </div>
+</article>
+<article id="lastEpisodeResponsive">
+  <div class="card">
+    <div class="row no-gutters">
+      <div class="col-6 offset-3 col-md-4 offset-md-0 couverture">
+        <img src="Public/images/couverture.jpg" class="card-img" alt="livre de Jean Forteroche">
+      </div>
+      <div class="col-8 offset-2 col-md-8col-md-4 offset-md-0 ">
+        <div class="card-body">
+          <h3 class="card-title"><?= htmlspecialchars($lastEpisode['title']) ?></h3>
+          <p class="card-text"><?= htmlspecialchars(substr($lastEpisode['content'], 0, 550)) ?> ... <a href="<?= 'index.php?action=episode&id=' . htmlspecialchars($lastEpisode['id']) ?>">lire la suite</a></p>
+          <p class="card-text"><small class="text-muted">Publié par Jean Forteroche le <?= htmlspecialchars($lastEpisode['creationFrDate']) ?></small></p>
+        </div>
+      </div>
+    </div>
+  </div>
 </article>
 <?php endforeach; ?>
-<section>
+<section id="lastNovels">
   <h2>Mes derniers récits</h2>
-  <ul>
-    <li>
-      <figure>
-        <a href="#"><img src="Public/images/aLaRechercheDuBonheur.png" alt="livre de Jean Forteroche" /></a>
-        <figcaption>
-
-        </figcaption>
-      </figure>
-    </li>
-    <li>
-      <figure>
-        <a href="#"><img src="Public/images/elleALaPecheMamie.png" alt="livre de Jean Forteroche" /></a>
-        <figcaption>
-
-        </figcaption>
-      </figure>
-    </li>
-    <li>
-      <figure>
-        <a href="#"><img src="Public/images/elleEstDailleurs.png" alt="livre de Jean Forteroche" /></a>
-        <figcaption>
-
-        </figcaption>
-      </figure>
-    </li>
-    <li>
-      <figure>
-        <a href="#"><img src="Public/images/jeLaimeAMourir.png" alt="livre de Jean Forteroche" /></a>
-        <figcaption>
-
-        </figcaption>
-      </figure>
-    </li>
-    <li>
-      <figure>
-        <a href="#"><img src="Public/images/leHurlement.png" alt="livre de Jean Forteroche" /></a>
-        <figcaption>
-
-        </figcaption>
-      </figure>
-    </li>
-    <li>
-      <figure>
-        <a href="#"><img src="Public/images/jeLuiDiraiDesMotsBleus.png" alt="livre de Jean Forteroche" /></a>
-        <figcaption>
-
-        </figcaption>
-      </figure>
-    </li>
-    <li>
-      <figure>
-        <a href="#"><img src="Public/images/jiraiTeDecrocherLaLune.png" alt="livre de Jean Forteroche" /></a>
-        <figcaption>
-
-        </figcaption>
-      </figure>
-    </li>
-    <li>
-      <figure>
-        <a href="#"><img src="Public/images/laTeteDansLesEtoiles.png" alt="livre de Jean Forteroche" /></a>
-        <figcaption>
-
-        </figcaption>
-      </figure>
-    </li>
-    <li>
-      <figure>
-        <a href="#"><img src="Public/images/lesRevesDeSophie.png" alt="livre de Jean Forteroche" /></a>
-        <figcaption>
-
-        </figcaption>
-      </figure>
-    </li>
-    <li>
-      <figure>
-        <a href="#"><img src="Public/images/unTangoAHuwan.png" alt="livre de Jean Forteroche" /></a>
-        <figcaption>
-
-        </figcaption>
-      </figure>
-    </li>
-    <li>
-      <figure>
-        <a href="#"><img src="Public/images/vousEtesTresBeau.png" alt="livre de Jean Forteroche" /></a>
-        <figcaption>
-
-        </figcaption>
-      </figure>
-    </li>
-    <li>
-      <figure>
-        <a href="#"><img src="Public/images/wilfriedEtLesLucioles.png" alt="livre de Jean Forteroche" /></a>
-        <figcaption>
-
-        </figcaption>
-      </figure>
-    </li>
-  </ul>
+  <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4">
+    <div class="col">
+      <div class="card h-100">
+        <img src="Public/images/aLaRechercheDuBonheur.png" class="card-img-top" alt="livre de Jean Forteroche">
+        <div class="card-body">
+          <h5 class="card-title">A la Recherche du bonheur</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card h-100">
+        <img src="Public/images/elleALaPecheMamie.png" class="card-img-top" alt="livre de Jean Forteroche">
+        <div class="card-body">
+          <h5 class="card-title">Elle a la pêche mamie</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card h-100">
+        <img src="Public/images/elleEstDailleurs.png" class="card-img-top" alt="livre de Jean Forteroche">
+        <div class="card-body">
+          <h5 class="card-title">Elle est d'ailleurs</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card h-100">
+        <img src="Public/images/jeLaimeAMourir.png" class="card-img-top" alt="livre de Jean Forteroche">
+        <div class="card-body">
+          <h5 class="card-title">Je l'aime à mourir</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col imageResponsive">
+      <div class="card h-100">
+        <img src="Public/images/jeLuiDiraiDesMotsBleus.png" class="card-img-top" alt="livre de Jean Forteroche">
+        <div class="card-body">
+          <h5 class="card-title">Elle est d'ailleurs</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col imageResponsive">
+      <div class="card h-100">
+        <img src="Public/images/jiraiTeDecrocherLaLune.png" class="card-img-top" alt="livre de Jean Forteroche">
+        <div class="card-body">
+          <h5 class="card-title">Je l'aime à mourir</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
